5642c3cbb20cb8f90a73349ee0cd84290c98a04a
justyman <justyman@163.com>
2018-08-29 11:38:06 +0800

    Initial commit

---
+++ src/main/java/com/gzb/registercenter/RegistercenterApplication.java
@@ -0,0 +1,16 @@
+package com.gzb.registercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class RegistercenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistercenterApplication.class, args);
+	}
+}
